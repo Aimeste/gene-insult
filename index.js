d587a57302926bca0d74802211c0a018ec63c4cb
Xavier Gruber <xavier.gruber@hotmail.com>
2017-05-23 18:26:55 +0200

    base js

---
+++ index.js
@@ -0,0 +1,5 @@
+var adjectif = ["vieille", "grosse", "sale"];
+var objet = ["chaussette", "patate", "armoire"];
+var compliment = ["molle", "pourrie", "malodorante"];
+
+alert("Espèce de " + adjectif[getRandomNumber(adjectif.length)] + " " + objet[getRandomNumber(objet.length)] + " " + compliment[getRandomNumber(compliment.length)];
